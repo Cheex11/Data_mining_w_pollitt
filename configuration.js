caf4a1a14c8a812bad0181c7731a76b398c91a5e
Cody Wilson <Cheex11@gmail.com>
2015-06-07 15:06:50 -0500

    Initial Commit
    Working with Pollitt to mine some info from my employees

---
+++ configuration.js
@@ -0,0 +1,11 @@
+var googleSpreadsheetURL = "https://docs.google.com/a/centuryinteractive.com/spreadsheets/d/11cvAkIFLJ-e6z2v1IybLAlfZvosPbff3GgiHMgHwtPE/edit?usp=sharing";  /* URL OF THE GOOGLE SPREADSHEET */
+
+var pageTitle_str = "Centruy Interactive Employees";	/* TITLE OF THE PAGE */
+var overIndexThreshold_num = 125;  /* MINIMUM INDEX REQUIRED FOR OVERINDEXING */
+var underIndexThreshold_num = 50;  /* MAXIMUM INDEX REQUIRED FOR UNDERINDEXING */
+var sampleSizePercentageThreshold_num = 2; /* MINIMUM OVERALL PERCENTAGE FOR INCLUSION IN DATA COMPARISONS */
+var strongCorrelationThreshold_num = 0.75; /* BASELINE FOR 'STRONG' AND 'STRONG INVERSE' CORRELATION */
+var moderateCorrelationThreshold_num = 0.4; /* BASELINE FOR 'MODERATE' AND 'MODERATE INVERSE' CORRELATION */
+var variableInterestingThreshold_num = 7; /* THRESHOLD FOR MEASURE OF 'INTERESTING'. HIGHER THRESHOLD = SMALLER LIST */
+var variableInterestingMaxCount_num = 5; /* MAXIMUM NUMBER OF 'INTERESTING' ITEMS CALLED OUT */
+var snapshotThreshold_num = 50; /* % OF POPULATION REQUIRED TO ASCRIBE A VARIABLE TO THE 'SNAPSHOT' SECTION */
---
+++ data/script.vbs
@@ -0,0 +1,17 @@
+dim ColumnHeaders
+ColumnHeaders = Array("Travel","Exercise","BeiberRespect","Confidence","Spirituality","Extroversion","CatLove","Drinker","Sleeper","SamHater")
+
+dim ColumnMetaHeaders
+ColumnMetaHeaders = Array("Traveler","Exercise","Beileber","Confidence","Spirituality","Extroversion","AnimalPreference","Drinker","Sleeper","SamOpinion")
+
+dim ColumnModifiers
+ColumnModifiers = Array("No","Light","Moderate","Maximum")
+
+
+
+Set objExcel = CreateObject("Excel.Application") 
+objExcel.Visible = True     
+	set workbook_data = objExcel.Workbooks.Open("C:\Users\Cody\Desktop\pollitt-master - Copy\data\data.xlsx") 
+	set workbook_fill = objExcel.Workbooks.Open("C:\Users\Cody\Desktop\pollitt-master - Copy\data\Employees.xlsx") 
+
+
